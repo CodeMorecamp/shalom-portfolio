6167d9a62994b7edf0b790378b788526d334f389
CodeMorecamp <71688722+CodeMorecamp@users.noreply.github.com>
2023-01-15 15:07:04 +0100

    Nav Components and styles

--- src/sections/navbar/NavBar.jsx
+++ src/sections/navbar/NavBar.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
-
+import './navbar.css'
+import logo from '../../asset/woman.jpg'
+import data from './data'
+import {IoIosColorPalette} from 'react-icons/io'
 const NavBar = () => {
   return (
-    <div>
-        <nav>Nav</nav>
+   <nav>
+    <div className='container nav__container'>
+      <a href='index.html' className='nav__logo'>
+      <img src={logo} alt='img'/>
+      </a>
+      <ul className='nav__menu'>
+      {
+        data.map(item => <li key={item.id}><a href={item.link}>{item.title}</a></li>)
+      }
+      </ul>
+      <button id='theme__icon'><IoIosColorPalette/></button>
     </div>
+   </nav>
   )
 }
 
