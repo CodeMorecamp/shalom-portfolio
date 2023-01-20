6cd8a7f9f5623ca4d6e70bd62504926858cb73fa
CodeMorecamp <71688722+CodeMorecamp@users.noreply.github.com>
2023-01-20 15:19:14 +0100

    Header Component and Header styles

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
-
+import HeaderImage from '../../asset/woman.jpg'
+import './header.css'
+import data from './data'
 const Header = () => {
   return (
     <div>
         <section id="header">
-            Header
+            <div className="container header__container">
+              <div className="header__profile">
+                <img src= {HeaderImage} alt='Header-img' />
+              </div>
+              <h3>I am Shalom</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis nobis aperiam voluptas consectetur, ullam amet veniam, iste qui accusamus molestias fugit 
+                ratione voluptatum possimus a! Dicta consequuntur iure autem perspiciatis?
+              </p>
+              <div className="header__cta">
+                <a href="#contact" className='btn primary'>Reach Out</a>
+                <a href="#portfolio" className='btn primary'>My Works</a>
+              </div>
+
+            <div className="header__socials">
+              {
+                data.map(item => <a key={item.id} href={item.link} target="_blank" rel= "noopener noreferrer">{item.icon}</a>)              }
+            </div>
+            </div>
         </section>
     </div>
   )
